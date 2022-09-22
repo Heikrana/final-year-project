4e884776fe44a30d30c488138dfe6da975e3c75d
Heikrana <49754221+Heikrana@users.noreply.github.com>
2022-09-23 01:10:31 +0530

    Add loader

--- components/navbutton.tsx
+++ components/navbutton.tsx
@@ -10,9 +10,9 @@ function NavButton({ title, isActive, onClick }: Props) {
 	return (
 		<button
 			onClick={onClick}
-			className={`${
-				isActive && "text-gray-800 bg-[#87A2FB]"
-			} text-stone-200 hover:bg-[#87A2FB] hover:text-gray-800 font-medium py-2 px-4 rounded`}
+			className={`${isActive && "bg-[#87A2FB] text-gray-800"} ${
+				!isActive && "text-stone-300"
+			} hover:bg-[#87A2FB] hover:text-gray-800 font-medium py-2 px-4 rounded`}
 		>
 			{title}
 		</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,34 @@
-import { useAddress } from "@thirdweb-dev/react";
+import { useAddress, useContract } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/header";
 import Login from "../components/login";
+import Logo from "../public/Logo.jpg";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Home: NextPage = () => {
 	const address = useAddress();
+	const { contract, isLoading } = useContract(
+		process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS
+	);
+
+	if (isLoading)
+		return (
+			<div className="bg-[#16213E] h-screen flex flex-col items-center justify-center">
+				<div className="flex items-center space-x-10 mb-10">
+					<img
+						className="rounded-full h-20 w-20"
+						src={Logo}
+						alt="Logo"
+					/>
+					<h1 className="text-lg text-white font-bold">
+						Loading the Raffle, hold on tight!
+					</h1>
+				</div>
+				<ClipLoader color="white" size={90} speedMultiplier={1.5} />
+			</div>
+		);
+
 	if (!address) return <Login />;
 
 	return (
